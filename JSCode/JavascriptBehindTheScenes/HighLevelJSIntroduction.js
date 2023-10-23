861624c7c181eb164f36afa9b61564f286861eac
SurajSingh <surajsinghj654@gmail.com>
2023-10-23 10:35:37 +0530

    High Level JS Introduction Completed

---
+++ JSCode/JavascriptBehindTheScenes/HighLevelJSIntroduction.js
@@ -0,0 +1,24 @@
+// What is JavaScript?
+// JavaScript is a high-level, object-oriented, multi-paradigm programming language.
+// High-level: It is a language that is close to human language.
+// Object-oriented: It is a language that is based on objects.
+// Multi-paradigm: It is a language that supports different programming paradigms.
+// Programming paradigms: It is a way of thinking about software construction based on some fundamental, defining principles.
+// Examples of programming paradigms: procedural programming, object-oriented programming, functional programming, etc.
+// Procedural programming: It is a programming paradigm based on the concept of the procedure call.
+// Procedure: It is a set of instructions that performs a specific task.
+// Object-oriented programming: It is a programming paradigm based on the concept of objects.
+// Object: It is a collection of related data and/or functionality (which usually consists of several variables and functions — which are called properties and methods when they are inside objects).
+// Functional programming: It is a programming paradigm based on the concept of functions.
+// Function: It is a set of statements that performs a specific task or calculates a value.
+// Examples of high-level programming languages: JavaScript, Python, PHP, Ruby, etc.
+// Examples of low-level programming languages: Assembly, C, C++, etc.
+
+// High-Level vs Low-Level
+// High-level: It is a language that is close to human language.Developer doesn't need to worry , everything happens automatically.
+// Low-level: It is a language that is close to machine language.Developer needs to worry about memory management, etc.Developer has to manage resources manually.
+
+// Garbage-collected: Cleans the memory automatically
+// Paradigm: An approach and mindset of structuring code , which will direct your coding style and technique.
+// First-class function: Simply treated as variable
+// Event loop: By using an event loop, takes long running tasks , executes them in the 'background' and puts them back in the main thread once they are finished!
